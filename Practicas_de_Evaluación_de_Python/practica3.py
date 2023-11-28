0591a437ab51859d9ed61294d9f961d7f1bc4d7c
Jose Antonio Benítez Ruíz <91873627+jbenrui@users.noreply.github.com>
2023-11-28 08:40:27 +0100

    Add files via upload

--- Practicas_de_Evaluación_de_Python/practica3.py
+++ Practicas_de_Evaluación_de_Python/practica3.py
@@ -1,14 +1,32 @@
-# Realiza, utilizando Python 3, el ejercicio 3 de la página 35 del libro “Introducción a
-# Python” de Jon Vadillo e inclúyelo en un fichero llamado lista.py. Las funciones que
+# Realiza, utilizando Python 3, el ejercicio 3 de la pagina 35 del libro "Introduccion a
+# Python de Jon Vadillo e inclúyelo en un fichero llamado lista.py. Las funciones que
 # debes usar en el ejercicio 3 deben utilizar OBLIGATORIAMENTE las siguientes
 # cabeceras:
 # def estaEnRango(valor, minimo, maximo)
-#   Devuelve True o False determinando que valor se encuentra entre el mínimo y el
-#   máximo.
+#   Devuelve True o False determinando que valor se encuentra entre el minimo y el
+#   maximo.
 # def estaEnLista(valor, lista)
-#   Devuelve True o False determinando si el valor está en la lista.
+#   Devuelve True o False determinando si el valor esta en la lista.
 
 def estaEnRango(valor, minimo, maximo):
-    return True 
+    return valor >= minimo & valor <= maximo 
 def estaEnLista(valor, lista):
-    return True 
+    return valor in lista 
+
+lista_numeros = [6, 14, 11, 3, 2, 1, 15, 19]
+
+while True:
+    try:
+        num = int(input("Introduce un número del 1 al 20: "))
+        if  not estaEnRango(num,1,20):
+            print("El número debe estar entre 1 y 20.")
+            continue
+            
+        if estaEnLista(num, lista_numeros):
+            print(f"El número {num} esta en la lista.")
+        else:
+            print(f"El número {num} no esta en la lista.")
+            
+        break
+    except ValueError:
+        print("Por favor, introduce un número valido.")
